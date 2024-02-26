f35da2095511b6e2a28b8c409dd9ba2752335e5a
Osama <107386980+osSidd@users.noreply.github.com>
2024-02-26 20:47:39 +0530

    initial

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP tutorial</h1>
+    <?php
+        //This is a single line comment
+        /*This ia a multi line 
+        comment spread over
+        multiple lines*/
+        echo "PHP is acronym for PHP Hypertext Preprocessor, created by Leadorf"
+    ?>
+</body>
+</html>
