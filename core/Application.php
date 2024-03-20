bfbe5ff88851898fd1c2098a34b136ba3af1cb7e
Osama <107386980+osSidd@users.noreply.github.com>
2024-03-20 13:30:53 +0530

    implemented views and status code

--- core/Application.php
+++ core/Application.php
@@ -3,16 +3,23 @@
     namespace app\core; //namespace declaration must be the first thing in a php file
 
     class Application{
+        public static string $ROOT_DIR;
+        public static Application $app;
         public Router $router;
         public Request $request;
-        public function __construct() 
+        public Response $response;
+
+        public function __construct($root_path) 
         {
+            self::$ROOT_DIR = $root_path;
+            self::$app = $this;
             $this->request = new Request();
-            $this->router = new Router($this->request);
+            $this->response = new Response();
+            $this->router = new Router($this->request, $this->response);
         }
 
         public function run()
         {
-            $this->router->resolve();
+            echo $this->router->resolve();
         }
     }
--- core/Router.php
+++ core/Router.php
@@ -1,14 +1,15 @@
 <?php
-
     namespace app\core;
 
     class Router{
-
         protected array $routes = [];
         public Request $request;
-        public function __construct(\app\core\Request $request)
+        public Response $response;
+
+        public function __construct(Request $request, Response $response)
         {
             $this->request = $request;
+            $this->response = $response;
         }
 
         public function get($path, $callback)
@@ -23,14 +24,36 @@
 
            $callback = $this->routes[$method][$path] ?? false;
 
-           if(!$callback)
-            {
-                echo "Not found";
-            }
-            else
-            {
-                echo call_user_func($callback);
-            }
+           if(!$callback){
+                // Application::$app->response->setStatusCode(404);
+                $this->response->setStatusCode(404);
+                return "Not found";
+           }
+
+           if(is_string($callback)) return $this->renderView($callback);
+        
+           else return call_user_func($callback);
+        }
+
+        public function renderView($view)
+        {
+            $layout_content = $this->layoutContent();
+            $view_content = $this->renderOnlyView($view);
+            return str_replace('{{content}}', $view_content, $layout_content);
+        }
+
+        protected function layoutContent()
+        {
+            ob_start();
+            include_once Application::$ROOT_DIR."/views/layout/main.php";
+            return ob_get_clean();
+        }
+
+        protected function renderOnlyView($view)
+        {
+            ob_start();
+            include_once Application::$ROOT_DIR."/views/$view.php";
+            return ob_get_clean();
         }
 
     }
